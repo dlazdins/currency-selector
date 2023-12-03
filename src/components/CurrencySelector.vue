cd7a80a26c584ac251f47509a3597041b3239a7c
dlazdins <davislazdins@gmail.com>
2023-12-03 18:53:52 +0200

    Update components styles

--- src/components/CurrencySelector.vue
+++ src/components/CurrencySelector.vue
@@ -1,6 +1,6 @@
 <template>
-	<div class="currency-selector-container">
-		<div class="currency-selector">
+	<div class="currency-selector">
+		<div class="content">
 			<!-- Selected currencies list -->
 			<div class="selected-currencies">
 				<div
@@ -8,8 +8,10 @@
 					:key="currency"
 					class="selected-currency"
 				>
-					{{ currency }}
-					<button @click="removeCurrency(index)">×</button>
+					<div>{{ currency }}</div>
+					<button class="close-button" @click="removeCurrency(index)">
+						<div>x</div>
+					</button>
 				</div>
 			</div>
 
@@ -19,16 +21,13 @@
 					v-for="currency in currencyOptions"
 					:key="currency"
 					class="currency-option"
+					:class="{ selected: isSelected(currency) }"
+					@click="toggleCurrency(currency)"
 				>
-					<label :class="{ 'is-selected': isSelected(currency) }">
-						<input
-							type="checkbox"
-							:value="currency"
-							@change="toggleCurrency(currency)"
-						/>
-						{{ currency }}
-						<span class="checkmark">x</span>
-					</label>
+					<div class="checkmark">
+						<div class="filler" v-if="isSelected(currency)">x</div>
+					</div>
+					<div>{{ currency }}</div>
 				</div>
 			</div>
 		</div>
@@ -50,6 +49,7 @@ export default class CurrencySelector extends Vue {
 		"PLN",
 		"DKK",
 		"SEK",
+		"GEL",
 	];
 
 	// The list of selected currencies
@@ -78,69 +78,117 @@ export default class CurrencySelector extends Vue {
 </script>
 
 <style lang="scss" scoped>
-.currency-selector-container {
+.currency-selector {
 	display: flex;
 	align-items: center;
 	justify-content: center;
 	height: 100%;
 	padding: 24px 24px;
-}
 
-.currency-selector {
-	border: 1px solid grey;
-	padding: 24px 12px 12px 12px;
-	border-radius: 4px;
-	width: 100%;
-	max-width: 500px;
-	margin: 0 auto;
+	.content {
+		border: 1px solid grey;
+		padding: 20px 12px 12px 12px;
+		border-radius: 4px;
+		width: 100%;
+		max-width: 470px;
+		margin: 0 auto;
+	}
 }
 
 .selected-currencies,
 .currency-options {
-	display: grid;
-	grid-template-columns: repeat(auto-fit, minmax(140px, 1fr));
+	display: flex;
+	flex-wrap: wrap;
+}
+
+.selected-currencies {
+	margin-bottom: 12px;
 }
 
 .currency-option {
-	color: navy;
+	display: flex;
+	align-items: center;
+	color: #305982;
+	font-weight: bold;
 	margin: 6px 4px;
 	border: 1px solid grey;
-	background-color: lightgray;
-	padding: 4px 12px;
+	background-color: #ececec;
+	padding: 4px 4px;
 	margin-top: 12px;
-}
+	width: 100%;
+	max-width: calc(33.3% - 8px);
+	cursor: pointer;
+	user-select: none;
 
-.selected-currency {
-	text-align: center;
-	margin: 6px 4px;
-	background-color: lightgray;
-	padding: 4px 12px;
-}
+	&.selected {
+		background-color: white;
+	}
 
-.selected-currency button {
-}
+	&.selected:hover {
+		background-color: white;
+	}
 
-.currency-option label {
-}
+	&:hover {
+		background-color: #dcdcdc;
+	}
 
-.currency-option input[type="checkbox"] {
-	/* Hide the default checkbox */
-	opacity: 0;
-	position: absolute;
-}
+	&:focus {
+		background-color: white;
+	}
 
-.currency-option .checkmark {
-}
+	.checkmark {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		border-radius: 4px;
+		border: 1px solid black;
+		width: 14px;
+		height: 14px;
+		margin-right: 4px;
 
-@media screen and (min-width: 750px) {
-	.grid-container {
-		grid-template-columns: repeat(3, 1fr);
+		.filler {
+			color: red;
+			font-size: 16px;
+			line-height: 1;
+			padding-bottom: 2px;
+		}
 	}
 }
 
-@media screen and (max-width: 499px) {
-	.grid-container {
-		grid-template-columns: 1fr; /* All items in one column on very small screens */
+.selected-currency {
+	position: relative;
+	text-align: center;
+	margin: 6px 4px;
+	background-color: #ececec;
+	padding: 4px 12px;
+	width: 100%;
+	max-width: calc(33.3% - 8px);
+
+	.close-button {
+		position: absolute;
+		top: -6px;
+		right: -6px;
+		border-radius: 50%;
+		border: 2px solid white;
+		background-color: black;
+		color: white;
+		width: 20px;
+		height: 20px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		cursor: pointer;
+
+		&:hover {
+			border: 2px solid black;
+			background-color: white;
+			color: black;
+		}
+
+		div {
+			line-height: 1;
+			padding-bottom: 2px;
+		}
 	}
 }
 </style>
